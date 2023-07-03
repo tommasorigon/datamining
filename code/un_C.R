7875c75f1fde6ec995c2f06169791cf8cbcc565f
Tommaso Rigon <tommaso.rigon@gmail.com>
2023-07-03 09:42:00 +0200

    asd

--- code/un_C.R
+++ code/un_C.R
@@ -1,22 +1,24 @@
 ## ----r------------------------------------------------------------------------
+library(plotly)
+p <- plot_ly(z = volcano, type = "surface")
+p
+
+
+## ----r------------------------------------------------------------------------
 library(ISLR)
 library(tidyverse)
 data(Hitters)
 Hitters <- na.omit(Hitters)
-Hitters <- mutate(Hitters, Salary = log10(Salary))
+Hitters <- mutate(Hitters, Salary = log10(Salary), Years = log(Years))
 
 
 ## ----r------------------------------------------------------------------------
 library(leaps)
-fit <- regsubsets(Salary ~ ., data = Hitters, method = "exhaustive", nbest = 40, nvmax = 20)
+fit <- regsubsets(Salary ~ ., data = Hitters, method = "exhaustive", nbest = 1, nvmax = 20)
 sum1 <- summary(fit)
-plot(rowSums(sum1$which), sum1$bic)
 
+plot(rowSums(sum1$which), sum1$cp)
 
-## ----r------------------------------------------------------------------------
-library(tidymodels)
-data(ames)
-ames <- mutate(ames, Sale_Price = log10(Sale_Price))
-fit <- regsubsets(Sale_Price ~ ., data = ames, method = "seqrep", nbest = 10, nvmax = 300)
+fit <- regsubsets(Salary ~ ., data = Hitters, method = "backward", nbest = 1, nvmax = 20)
 sum1 <- summary(fit)
-plot(rowSums(sum1$which), sum1$bic)
+# plot(rowSums(sum1$which), sum1$cp)
