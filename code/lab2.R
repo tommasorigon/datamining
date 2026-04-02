f5cf558ed97e13805321f1ce9c5368f26718d98d
Tommaso Rigon <tommaso.rigon@gmail.com>
2026-04-02 11:16:18 +0200

    Update lab2.R

--- code/lab2.R
+++ code/lab2.R
@@ -29,11 +29,13 @@ plot(trawl_tr$Longitude, trawl_tr$Score1, pch = 16)
 plot(trawl_val$Longitude, trawl_val$Score1, pch = 16)
 
 # Fitting a polynomial regression "the old way" vs better approach
-m_degree3_raw <- lm(Score1 ~ Longitude + I(Longitude^2) + I(Longitude^3),
+m_degree3_raw <- lm(
+  Score1 ~ Longitude + I(Longitude^2) + I(Longitude^3),
   data = trawl_tr
 )
 
-m_degree3_poly <- lm(Score1 ~ poly(Longitude, 3),
+m_degree3_poly <- lm(
+  Score1 ~ poly(Longitude, 3),
   data = trawl_tr
 )
 
@@ -43,12 +45,14 @@ rmse_vec(trawl_val$Score1, fit_degree3)
 
 # The tidymodel workflow ------------------------------------------------------------------------------
 
-# Recipe (defined on training and validation data)
+# Recipe (defined on training data)
 rec_poly_3 <- recipe(Score1 ~ Longitude, data = trawl_tr) %>%
   step_poly(Longitude, degree = 3)
 
-bake(prep(rec_poly_3), new_data = NULL) # Training data
-bake(prep(rec_poly_3), new_data = head(trawl_val)) # Validation data
+rec_prep <- prep(rec_poly_3)
+
+bake(rec_prep, new_data = NULL) # Training data
+bake(rec_prep, new_data = head(trawl_val)) # Validation data
 
 # Tunable recipe
 rec_poly <- recipe(Score1 ~ Longitude, data = trawl_tr) %>%
@@ -69,8 +73,8 @@ grid_poly <- tibble(degree = 1:15)
 
 # Tuning on validation set (train + validation only)
 poly_val <- tune_grid(
-  m_linear,
-  rec_poly,
+  object = m_linear,
+  preprocessor = rec_poly,
   resamples = val_resample,
   metrics = metric_list,
   grid = grid_poly,
@@ -96,9 +100,10 @@ trawl_tr2 <- bind_rows(trawl_tr, trawl_val)
 cv_samples <- vfold_cv(trawl_tr2, v = 10)
 
 # Create an object that will be used in the workflow
-poly_cv <- tune_grid(m_linear,
-  rec_poly,
-  resamples = cv_samples, # This guarantees we use train and validation (not test!)
+poly_cv <- tune_grid(
+  object = m_linear,
+  preprocessor = rec_poly,
+  resamples = cv_samples, # uses train + validation (not test)
   metrics = metric_list,
   grid = grid_poly,
   control = control_grid(save_workflow = TRUE, verbose = TRUE)
@@ -115,14 +120,19 @@ select_best(poly_cv, metric = "mae")
 best_lm_cv <- fit_best(poly_cv, metric = "rmse")
 
 # Final errors
-rmse_vec(truth = trawl_te$Score1, 
-         estimate = predict(best_lm_val, new_data = trawl_te)$.pred)
-rmse_vec(truth = trawl_te$Score1, 
-         estimate = predict(best_lm_cv, new_data = trawl_te)$.pred)
+rmse_vec(
+  truth = trawl_te$Score1,
+  estimate = predict(best_lm_val, new_data = trawl_te)$.pred
+)
 
-# Graphical representation on the validation
+rmse_vec(
+  truth = trawl_te$Score1,
+  estimate = predict(best_lm_cv, new_data = trawl_te)$.pred
+)
+
+# Graphical representation on the test set
 seq_data <- tibble(Longitude = seq(142.5, 144, length.out = 200))
+
 plot(trawl_te$Longitude, trawl_te$Score1, pch = 16)
 lines(seq_data$Longitude, predict(best_lm_val, seq_data)$.pred, col = "red")
 lines(seq_data$Longitude, predict(best_lm_cv, seq_data)$.pred)
-
